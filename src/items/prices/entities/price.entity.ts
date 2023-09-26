04777c82523c313044644bcbfa6d43dcaab76c99
mostafa nematpour <nematpor46@gmail.com>
2023-09-26 14:59:47 +0330

    prices bug-fix

--- src/items/prices/entities/price.entity.ts
+++ src/items/prices/entities/price.entity.ts
@@ -1,5 +1,5 @@
-import { Item } from 'src/items/entities/item.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Item } from 'src/items/entities/item.entity';
 
 @Entity()
 export class Price {
--- src/items/prices/prices.module.ts
+++ src/items/prices/prices.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PricesService } from './prices.service';
 import { PricesController } from './prices.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Price } from './entities/price.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Price])],
   controllers: [PricesController],
   providers: [PricesService],
 })
